e7c7f3da8ac46e896be3f6a3c8667f44d0d0c253
Aiden Fox <madelinemfox@gmail.com>
2014-12-04 02:29:14 -0500

    Update Professor.java
    JavaDoc'd

--- hw3/Professor.java
+++ hw3/Professor.java
@@ -1,12 +1,28 @@
+/**
+ * 
+ * @author Madeline Fox
+ * @version 1.0
+ */
 public class Professor extends Person {
     private int rateMyProfessorRating;
     private double averageGPA;
+    /**
+     * 
+     * @param firstName             -String representation of the first name
+     * @param lastName              -String representation of the last name
+     * @param rateMyProfessorRating -int representation of the Professor's rating
+     * @param averageGPA            -double representation of the average GPA
+     */
     public Professor(String firstName, String lastName,
       int rateMyProfessorRating, double averageGPA) {
         super(lastName, firstName);
         this.rateMyProfessorRating = rateMyProfessorRating;
         this.averageGPA = averageGPA;
     }
+    /**
+     * 
+     * @return String representation of the object.
+     */
     public String toString() {
         return ("Hi, my name is " + firstName + " " + lastName + ". My Rate My "
                     + "Professor rating is " + rateMyProfessorRating + "/5 and "
