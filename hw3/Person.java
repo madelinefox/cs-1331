081afaf3f367f77b8f38315ece51ed41bb54cc25
Aiden Fox <madelinemfox@gmail.com>
2014-12-04 02:19:25 -0500

    Update Person.java
    JavaDoc'd

--- hw3/Person.java
+++ hw3/Person.java
@@ -1,10 +1,25 @@
+/**
+ * 
+ * @author Madeline Fox
+ * @version 1.0
+ */
 public class Person {
     protected String firstName;
     protected String lastName;
+    /**
+     * 
+     * @param firstNam  -a String representation of the first name
+     * @param lastName  -a String representation of the last name
+     */
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+    /**
+     * 
+     * 
+     * @return String representation of this object
+     */
     public String toString() {
         return (firstName + lastName);
     }
