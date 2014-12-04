b41314960847e14178f4997b2c9ebc983e6ce9d5
Aiden Fox <madelinemfox@gmail.com>
2014-12-04 02:16:50 -0500

    Update GraduateStudent.java
    JavaDoc'd

--- hw3/GraduateStudent.java
+++ hw3/GraduateStudent.java
@@ -1,8 +1,24 @@
+/**
+ * 
+ * @author Madeline Fox
+ * @version 1.0
+ */
 public class GraduateStudent extends Student {
+    /**
+     * 
+     * @param firstName     -a String representation of first name
+     * @param lastName      -a String representation of last name
+     * @param intelligence  -an integer representation of intellegence
+     * @param motivation    -an integer representation of motivation
+     */
     public GraduateStudent(String firstName, String lastName, int intelligence,
       int motivation) {
         super(firstName, lastName, intelligence, motivation);
     }
+    /**
+      *
+      * @return String representation of the object.
+      */
     public String toString() {
         return ("Hi, my name is " + firstName + " " + lastName
                    + ". My intelligence is " + intelligence + "/10 and my motiv"
