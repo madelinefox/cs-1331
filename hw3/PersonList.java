3637095475a5fb57162ffcdef7381fc1b3aa9e7d
Aiden Fox <madelinemfox@gmail.com>
2014-12-04 02:22:51 -0500

    Update PersonList.java
    JavaDoc'd

--- hw3/PersonList.java
+++ hw3/PersonList.java
@@ -1,10 +1,21 @@
+/**
+ * 
+ * @author Madeline Fox
+ * @version 1.0
+ */
 public class PersonList {
     private Person[] people;
     private int count;
-
+    /**
+     * 
+     * @param maxSize - an integer to show what the max size of the backing array will be.
+     */
     public PersonList(int maxSize) {
         people = new Person[maxSize];
     }
+    /**
+     * Prints all people in the list backing array.
+     */
     public void listPeople() {
         for (int i = 0; i < people.length; i++) {
             if (people[i] != null) {
@@ -12,6 +23,10 @@ public class PersonList {
             }
         }
     }
+    /**
+     * Adds a Person to the backing array.
+     * @param p -a Person object to add to the list backing array
+     */
     public void add(Person p) {
         for (int i = 0; i < people.length; i++) {
             if (people[i] == null) {
