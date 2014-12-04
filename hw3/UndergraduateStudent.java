f0ba0f64eca4eedbf5be72341ab6a7355e71d8fd
Aiden Fox <madelinemfox@gmail.com>
2014-12-04 02:40:38 -0500

    Update UndergraduateStudent.java
    javadoc'd

--- hw3/UndergraduateStudent.java
+++ hw3/UndergraduateStudent.java
@@ -1,8 +1,24 @@
+/**
+ * 
+ * @author Madeline Fox
+ * @version 1.0
+ */
 public class UndergraduateStudent extends Student {
+    /**
+     * 
+     * @param firstName
+     * @param lastName
+     * @param intelligence
+     * @param motivation
+     */
     public UndergraduateStudent(String firstName, String lastName,
                                     int intelligence, int motivation) {
         super(firstName, lastName, intelligence, motivation);
     }
+    /**
+     * 
+     * @return String representation of the object
+     */
     public String toString() {
         return ("Hi, my name is " + firstName + " " + lastName
                     + ". My intelligence is " + intelligence + "/10 and my moti"
